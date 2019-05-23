8e91906001daa2a45b967fb00a9629a58da9a632
Alec <alec.jordan.cs@gmail.com>
2019-05-23 00:11:19 -0400

    implements store

--- basic/index.js
+++ basic/index.js
@@ -1,1 +1,6 @@
-import { createStore} from 'redux'
+import {createStore} from 'redux';
+import todoApp from './reducers';
+
+const store = createStore(todoApp);
+console.log('hello')
+console.log(store.getState());
